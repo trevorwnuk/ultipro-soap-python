c2d35dec6253a82b1dd75a09068884db55a44d8e
trevorwnuk <twnuk2039@live.com>
2019-05-16 12:27:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ UltiPro SOAP Python is a Python client that wraps the UltiPro SOAP API.
 '''
 
 setup(
-    name='ultipro',
+    name='ultipro2',
     version='0.0.2',
     url='https://github.com/call/ultipro-soap-python',
     author_email='bizappdev@puppet.com',
     packages=['ultipro', 'ultipro.services'],
     license='Apache License 2.0',
     install_requires=[
-        'backoff==1.4.3',
+        'backoff==1.3.2',
         'click==6.7',
         'configparser==3.5.0',
         'zeep==2.0.0'
